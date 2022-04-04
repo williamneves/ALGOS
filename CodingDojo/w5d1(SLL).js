78c4e161476debd81d4bc909c3f0ad2d58db83cc
William Neves <drwilliamneves@gmail.com>
2022-04-04 11:08:47 -0400

    SLL CD

---
+++ CodingDojo/w5d1(SLL).js
@@ -0,0 +1,19 @@
+// Node Class
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+// Singly LInked List
+
+class SLL{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    // Methods
+    // Add to front
+}
