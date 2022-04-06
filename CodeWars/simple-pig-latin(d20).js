b3375a4cebdb0f71c6628b836ee6307ff0d87492
William Neves <drwilliamneves@gmail.com>
2022-04-06 08:43:57 -0400

    d20update

--- CodeWars/simple-pig-latin(d20).js
+++ CodeWars/simple-pig-latin(d20).js
@@ -8,11 +8,19 @@ function pigIt(str){
     
     var list_words = str.split(' ');
 
+    var re = '[a-zA-Z]'
+
     for (var i = 0; i < list_words.length; i++) {
         var word = list_words[i];
-        var first_letter = word.substring(0,1);
-        var rest_word = word.substring(1, word.length);
-        list_words[i] = rest_word + first_letter + 'ay';
+        if (word.match(re)){
+            var first_letter = word.substring(0,1);
+            var rest_word = word.substring(1, word.length);
+            list_words[i] = rest_word + first_letter + 'ay';
+        }
+        else{
+
+            list_words[i] = word;
+        }
     }
 
     return list_words.join(' ');
